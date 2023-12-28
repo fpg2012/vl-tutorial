fe2050b3284872356a6c6982ba500faac4364ad8
unknown <fpg2012>
2023-12-28 15:45:57 +0800

    restart my learning

--- main.cpp
+++ main.cpp
@@ -1,104 +1,119 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#include <vulkan/vulkan.h>
-
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 #include <vector>
+#include <cstdlib>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+const uint32_t WIDTH = 800; // width of the window
+const uint32_t HEIGHT = 600; // height of the window
 
+// wrap all operations into this class
 class HelloTriangleApplication {
 public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
-
+	HelloTriangleApplication() = default;
+	void run();
 private:
-    GLFWwindow* window;
-    VkInstance instance;
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void cleanup();
 
-    void initWindow() {
-        glfwInit();
+	void checkSupportedExtensions(uint32_t glfwExtensionCount, const char **glfwExtentions);
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Tutorial", nullptr, nullptr);
-    }
-
-    void initVulkan() {
-        createInstance();
-    }
-
-    void createInstance() {
-
-        uint32_t extensionCount = 0;
-        // read available extension count
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-        // retrieve list of extensions
-        std::vector<VkExtensionProperties> extensions(extensionCount);
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-        std::cout << "available extensions:\n";
-        for (const auto& extension : extensions) {
-            std::cout << '\t' << extension.extensionName << '\n';
-        }
-
-        VkApplicationInfo appInfo{
-            .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-            .pApplicationName = "Hello Triangle",
-            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
-            .pEngineName = "No Engine",
-            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-            .apiVersion = VK_API_VERSION_1_0,
-        };
-
-        VkInstanceCreateInfo createInfo{
-            .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-            .pApplicationInfo = &appInfo,
-        };
-
-        uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
-        createInfo.enabledLayerCount = 0;
-
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create instance!");
-        }
-    }
-
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
-
-    void cleanup() {
-        vkDestroyInstance(instance, nullptr);
-
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+	GLFWwindow* window;
+	VkInstance instance;
 };
 
+using HTA = HelloTriangleApplication;
+
+void HTA::run() {
+	initWindow(); // init window with GLFW
+	initVulkan(); // init VkInstance, physical device and logical device
+	mainLoop();
+	cleanup();
+}
+
+// init VkInstance, physical device and logical device
+void HTA::initVulkan() {
+	VkApplicationInfo appInfo{
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = "Hello Triangle",
+		.applicationVersion = VK_MAKE_API_VERSION(1, 0, 0, 0),
+		.pEngineName = "No Engine",
+		.engineVersion = VK_MAKE_API_VERSION(1, 0, 0, 0),
+		.apiVersion = VK_API_VERSION_1_0,
+	};
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	checkSupportedExtensions(glfwExtensionCount, glfwExtensions);
+	
+	VkInstanceCreateInfo createInfo{
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &appInfo,
+		.enabledLayerCount = 0,
+		.enabledExtensionCount = glfwExtensionCount,
+		.ppEnabledExtensionNames = glfwExtensions,
+	};
+
+	VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+	if (result != VK_SUCCESS) {
+		throw std::runtime_error("failed to create instance!");
+	}
+}
+
+// a bunch of vkDestroy & vkFree functions
+void HTA::cleanup() {
+	vkDestroyInstance(instance, nullptr);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void HTA::mainLoop() {
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+}
+
+// init window with GLFW
+void HTA::initWindow() {
+	glfwInit();
+	std::cout << "vulkan support: " << (glfwVulkanSupported() ? "TRUE" : "FALSE") << std::endl;
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	// create a window titled "vulkan"
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
+void HTA::checkSupportedExtensions(uint32_t glfwExtensionCount, const char** glfwExtentions) {
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	std::cout << "available extensions(" << extensionCount << "):\n";
+	for (const auto& extension : extensions) {
+		std::cout << '\t' << extension.extensionName << '\n';
+	}
+
+	std::cout << "glfw extensions(" << glfwExtensionCount << "):\n";
+	for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+		std::cout << '\t' << glfwExtentions[i] << '\n';
+	}
+}
+
 int main() {
-    HelloTriangleApplication app;
+	HelloTriangleApplication app;
 
-    try {
-        app.run();
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
